94725ff30d8f18b70c171ce2da4744f755fa1d04
StefVanNood <vannoodstef@gmail.com>
2021-03-07 10:49:57 +0100

    base2

--- polyEngine/render.js
+++ polyEngine/render.js
@@ -16,30 +16,33 @@ document.querySelector("#read-button").addEventListener('click', function() {
 });
 
 function compile()
-{   
+{
     row = row.split(" ")
     object1.push(row)
     for (x = 1; x <= object1[0].length / 9; x++)
-        console.log(object1[0][8 + (9 * (x - 1))])
         draw(object1[0][0 + (9 * (x - 1))], 
-            object1[0][1 + (9 * (x - 1))], 
-            object1[0][2 + (9 * (x - 1))], 
-            object1[0][3 + (9 * (x - 1))], 
-            object1[0][4 + (9 * (x - 1))], 
-            object1[0][5 + (9 * (x - 1))], 
-            object1[0][6 + (9 * (x - 1))], 
-            object1[0][7 + (9 * (x - 1))], 
-            object1[0][8 + (9 * (x - 1))], "FF", "00", "00", 0, 0)
+        object1[0][1 + (9 * (x - 1))], 
+        object1[0][2 + (9 * (x - 1))], 
+        object1[0][3 + (9 * (x - 1))], 
+        object1[0][4 + (9 * (x - 1))], 
+        object1[0][5 + (9 * (x - 1))], 
+        object1[0][6 + (9 * (x - 1))], 
+        object1[0][7 + (9 * (x - 1))], 
+        object1[0][8 + (9 * (x - 1))], "FF", "00", "00", 1, 1)
 }
 
 function draw(x1, y1, z1, x2, y2, z2, x3, y3, z3, r, g, b, x, y)
-{
+{   
     context.beginPath();
-    context.moveTo(x1 + x, y1 + y);
-    context.lineTo(x2 + x, y2 + y);
-    context.lineTo(x3 + x, y3 + y);
+    context.moveTo(parseInt(x1) + x, parseInt(y1) + y);
+    context.lineTo(parseInt(x2) + x, parseInt(y2) + y);
+    context.lineTo(parseInt(x3) + x, parseInt(y3) + y);
     context.closePath();
+
+    context.lineWidth = 10;
+    context.strokeStyle = '#666666';
+    context.stroke();
+
     context.fillStyle = "#"+r+g+b;
     context.fill();
-    console.log("rendered")
 }
